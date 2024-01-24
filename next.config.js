1838baf650f14d6daee72a72b1545aa17685eb40
vitthalkokare <vitthalkokare777@gmail.com>
2024-01-24 16:03:37 +0530

    updaated

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:"export",
-    "rules": {
-        "react/no-unescaped-entities": "off",
-        "@next/next/no-page-custom-font": "off"
-      },
-      eslint: {
-        ignoreDuringBuilds: true,
-      },
+    
 }
 
 
