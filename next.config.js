11bbc56185198d322bed9d549acf6ab0d8b81498
vitthalkokare <vitthalkokare777@gmail.com>
2024-01-24 15:44:36 +0530

    nextconfig updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+    output:"export",
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
 }
+
 
 module.exports = nextConfig
