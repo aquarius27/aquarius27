e8ecf35879f79eaf8412ed5b9d43335ed2d05f82
vitthalkokare <vitthalkokare777@gmail.com>
2024-01-24 15:59:16 +0530

    eslintrc updated

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
     "rules": {
         "react/no-unescaped-entities": "off",
         "@next/next/no-page-custom-font": "off"
-      }
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
 }
 
 
