7751ab1bf4079fef54d67b9a6fc7e2afceaba397
vitthalkokare <vitthalkokare777@gmail.com>
2024-01-25 16:05:57 +0530

    'd'

--- src/components/Sociallink.tsx
+++ src/components/Sociallink.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
-import { FaGithub,FaLinkedinIn, } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa6';
+import { FaDownload, FaGithub,FaLinkedinIn, } from 'react-icons/fa';
+import { FaFileArrowDown, FaTwitter } from 'react-icons/fa6';
 import { SiLeetcode } from "react-icons/si";
 
 const SocilLinks: React.FC = () => {
@@ -11,10 +11,10 @@ const SocilLinks: React.FC = () => {
                 <li className='mx-1'><a href="https://github.com/vitthalkokare"target="_blank" rel="noopener noreferrer"><FaGithub/></a></li>
                 <li className='mx-1'><a href="https://leetcode.com/aquarius27/" target='_blank' rel='noopener noreferrer'><SiLeetcode/></a></li>
                 <li className='mx-1'><a href="https://twitter.com/search?q=%23tech&src=typed_query" target='_blank' rel='noopener noreferrer'><FaTwitter/></a></li>
-                <button className='bg-green-500 outline-none box-border font-semibold text-lg p-1 rounded-2xl'><a href="/vitthalkokare.pdf" download={'./vitthalkokare.pdf'}>Resume</a></button>
-
+                <button className='bg-green-500 outline-none hover:bg-green-600 box-border font-semibold text-lg p-1 rounded-2xl'><a className='flex items-center' href="/vitthalkokare.pdf" download={'/vitthalkokare.pdf'}>Resume <FaFileArrowDown/></a></button>
+               
             </div> 
-  )
+  ) 
 }
 
 export default SocilLinks
